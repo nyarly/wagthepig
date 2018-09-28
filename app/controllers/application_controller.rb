ffbd972001677aff58c69f9969e92f1859c20a88
Judson <nyarly@gmail.com>
2018-09-28 15:14:51 -0700

    Pleasant styling futzing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :route_vars
+
+  private
+
+  def route_vars
+    @controller = params[:controller]
+    @action = params[:action]
+  end
 end
