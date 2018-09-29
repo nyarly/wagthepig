73575aac1a23ebcf8b2559d294cac78ca8b7031c
Judson <nyarly@gmail.com>
2018-09-29 15:30:49 -0700

    Durations, minutes & seconds

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,12 @@ class Game < ApplicationRecord
   belongs_to :suggestor, class_name: 'User'
   has_many :interests
   has_many :users, through: :interests
+
+  def duration_minutes
+    duration_secs / 60
+  end
+
+  def duration_minutes=(n)
+    self.duration_secs = n * 60
+  end
 end
