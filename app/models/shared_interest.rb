28e18d8eca3a8f9a10800a5a57e931e7b5a9afd4
Judson <nyarly@gmail.com>
2018-11-11 16:27:08 -0800

    Added teachers to shared interest view

--- app/models/shared_interest.rb
+++ app/models/shared_interest.rb
@@ -2,7 +2,7 @@ class SharedInterest # not an ActiveRecord!
   include ActiveModel::Conversion
   extend ActiveModel::Naming
 
-  attr_reader :game_id, :users, :game
+  attr_reader :game_id, :users, :game, :teachers
 
   def initialize(game_id)
     @game_id = game_id
@@ -10,7 +10,8 @@ class SharedInterest # not an ActiveRecord!
 
   def query
     @game = Game.find(game_id)
-    @users = User.joins(interest: :game).where('games.id' => game_id).all
+    @users = User.joins(interest: :game).where('games.id' => game_id, 'interests.can_teach' => false).all
+    @teachers = User.joins(interest: :game).where('games.id' => game_id, 'interests.can_teach' => true).all
     self
   end
 end
