fa590fcf532da7211ac3bfcb152ca98655dc88dc
Judson <nyarly@gmail.com>
2018-11-11 17:02:00 -0800

    Adding teacher count and full interest to suggestions

--- app/models/suggestion.rb
+++ app/models/suggestion.rb
@@ -5,7 +5,6 @@ class Suggestion # not an ActiveRecord!
   attr_accessor :event_id, :user_ids, :extra_players, :users, :games
 
   def initialize(user_id, params)
-    Rails.logger.info(params)
     @event_id, @user_ids, @extra_players = params.values_at(:event_id, :user_ids, :extra_players)
     @current_user_id = user_id
     @extra_players = 0 unless @extra_players.present?
@@ -24,13 +23,16 @@ class Suggestion # not an ActiveRecord!
   def show_query
     user_ids = (@user_ids.select{|id| id != ""} + [@current_user_id]).uniq
     must_play = user_ids.length + extra_players.to_i
-    #@games = Game.joins(:event)
     @games = Game.joins(:event, :users)
-      .select("games.*, count('games.id') as interest_level")
+      .select(<<-QUERY)
+      games.*,
+      count('games.id') as interest_level,
+      count('games.id') FILTER (WHERE interests.can_teach is true) as teachers
+      QUERY
       .where('coalesce(games.max_players, 9999) >= ?', must_play)
       .where('events.id' => @event_id)
       .where('users.id' => user_ids)
-      .order('interest_level desc')
+      .order('interest_level desc, teachers desc')
       .group('games.id')
     @users = User.where(id: user_ids)
     self
