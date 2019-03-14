060a9e725f63bfb7b8485c7e6b03d229b54f8858
Evan Dorn <evan@idahoev.com>
2019-03-13 21:07:53 -0700

    add missing files for footer and responsive menu

---
+++ app/assets/javascripts/responsive.js
@@ -0,0 +1,29 @@
+// utilities
+var get = function (selector, scope) {
+  scope = scope ? scope : document;
+  return scope.querySelector(selector);
+};
+
+var getAll = function (selector, scope) {
+  scope = scope ? scope : document;
+  return scope.querySelectorAll(selector);
+};
+
+
+
+window.addEventListener('load', function(){
+  // responsive navigation
+  var topNav = get('.menu');
+  var icon = get('.toggle');
+
+  function showNav() {
+    if (topNav.className === 'menu') {
+      topNav.className += ' responsive';
+      icon.className += ' open';
+    } else {
+      topNav.className = 'menu';
+      icon.classList.remove('open');
+    }
+  }
+  icon.addEventListener('click', showNav);
+});
